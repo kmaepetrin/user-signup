df8d7e00d78bc83ba36e95e55773a6ac6c2f6d48
K Mae Petrin <kmaepetrin@gmail.com>
2017-10-01 18:52:50 -0500

    error messages written

--- main.py
+++ main.py
@@ -18,16 +18,23 @@ def input_data():
     verify_password = request.form['verify_password']
     email = request.form['email']
 
-    if username == "":
-        error = "Please submit a username"
-        return redirect("/?error=" + error)
-    elif " " in username: 
-        error = "Usernames cannot contain invalid characters"
-        return redirect("/?error=" + error)
-    elif len(username) < 3 or len(username) > 20:
-        error = "Usernames must be longer than 3 and shorter than 20 characters"
+    if username == "" or " " in username or len(username) < 3 or len(username) > 20:
+        error = "Please submit a valid username"
         return redirect("/?error=" + error)
 
+    if password == "" or " " in password or len(password) < 3 or len(password) > 20:
+        error = "Please submit a valid password"
+        return redirect("/?error=" + error)
+
+    if password != verify_password:
+        error = "Your passwords do not match"
+        return redirect("/?error=" + error)
+
+    if email != "":
+        if ("@" not in email) or ("." not in email) or (" " in email) or len(password) < 3 or len(password) > 20:
+            error = "Your email is invalid"
+            return redirect("/?error=" + error)
+    
     else:
         return render_template('welcome.html', username=username)
 
