fd87801829b3f6435590e551a332eba426786181
K Mae Petrin <kmaepetrin@gmail.com>
2017-10-01 19:10:35 -0500

    ifelse built

--- main.py
+++ main.py
@@ -18,29 +18,34 @@ def input_data():
     verify_password = request.form['verify_password']
     email = request.form['email']
 
-    if username == "" or " " in username or len(username) < 3 or len(username) > 20:
-        error = "Please submit a valid username"
-        return redirect("/?error=" + error)
+    if :
+        if username == "" or " " in username or len(username) < 3 or len(username) > 20:
+            username_error = "Please submit a valid username"
+        else:
+            username_error = ""
 
-    if password == "" or " " in password or len(password) < 3 or len(password) > 20:
-        error = "Please submit a valid password"
-        return redirect("/?error=" + error)
+        if password == "" or " " in password or len(password) < 3 or len(password) > 20:
+            password_error = "Please submit a valid password"
+        else:
+            password_error = ""
 
-    if password != verify_password:
-        error = "Your passwords do not match"
-        return redirect("/?error=" + error)
+        if password != verify_password:
+            verify_error = "Your passwords do not match"
+        else:
+            verify_error = ""
 
-    if email != "":
-        if ("@" not in email) or ("." not in email) or (" " in email) or len(password) < 3 or len(password) > 20:
-            error = "Your email is invalid"
-            return redirect("/?error=" + error)
-    
+        if email != "":
+            if ("@" not in email) or ("." not in email) or (" " in email) or len(password) < 3 or len(password) > 20:
+                email_error = "Your email is invalid"
+        else:
+            email_error = ""
+
+        return template_render('index.html', username_error=username_error, password_error=password_error, verify_error=verify_error, email_error=email_error)
     else:
         return render_template('welcome.html', username=username)
 
 @app.route("/", methods=["GET", 'POST'])
 def index():
-    encoded_error = request.args.get("error")
-    return render_template('index.html', error=encoded_error and cgi.escape(encoded_error, quote=True))
+    return render_template('index.html')
 
 app.run()
